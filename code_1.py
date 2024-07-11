29b86bb6884d5e47c771425a1d6c7850ca1e2fe6
Hansaka2001 <hansakarukshan43@gmail.com>
2024-07-11 22:41:25 +0530

    loading the dataset

--- code_1.py
+++ code_1.py
@@ -1,7 +1,15 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import preprocessing
+import tensorflow as tf
 import keras
-import tensorflow as tf 
+from sklearn import preprocessing
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+
+# Loading the dataset
+
+ipl = pd.read_csv('ipl_data.csv')
+print(ipl.head())
