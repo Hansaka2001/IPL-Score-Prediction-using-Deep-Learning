d135f6640bf2fc8ab70f1d0bb3b24d1f10b7dbcc
Hansaka2001 <hansakarukshan43@gmail.com>
2024-07-12 00:48:49 +0530

    before final testing

--- code_1.py
+++ code_1.py
@@ -1,53 +1,51 @@
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import warnings
+from IPython.display import display, clear_output
+import ipywidgets as widgets
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import tensorflow as tf
-import keras
-from sklearn import preprocessing
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-# Loading the dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+import keras
+import tensorflow as tf
 
 ipl = pd.read_csv('ipl_data.csv')
 ipl.head()
 
-# Data Preprocessing
-
-# Droping unimportant features
+# Dropping certain features
 df = ipl.drop(['date', 'runs', 'wickets', 'overs', 'runs_last_5',
-              'wickets_last_5', 'mid', 'striker', 'non-striker'], aixs=1)
+              'wickets_last_5', 'mid', 'striker', 'non-striker'], axis=1)
 
-# furter preprocessing
-x = df.drop(['total'], axis=1)
+X = df.drop(['total'], axis=1)
 y = df['total']
 
-# label encoding
+# Label Encoding
 
 
-# create a labelEncoder object for each categorial feature
+# Create a LabelEncoder object for each categorical feature
 venue_encoder = LabelEncoder()
 batting_team_encoder = LabelEncoder()
 bowling_team_encoder = LabelEncoder()
 striker_encoder = LabelEncoder()
 bowler_encoder = LabelEncoder()
 
-# fit and transform the categorial features with label encoding
+# Fit and transform the categorical features with label encoding
 X['venue'] = venue_encoder.fit_transform(X['venue'])
-X['batting_team'] = batting_team_encoder.fit_transform(X['batting_team'])
-X['bowling_team'] = bowling_team_encoder.fit_transform(X['bowling_team'])
+X['bat_team'] = batting_team_encoder.fit_transform(X['bat_team'])
+X['bowl_team'] = bowling_team_encoder.fit_transform(X['bowl_team'])
 X['batsman'] = striker_encoder.fit_transform(X['batsman'])
 X['bowler'] = bowler_encoder.fit_transform(X['bowler'])
+
 
 # Train test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42)
 
-# feature scaling
 
 scaler = MinMaxScaler()
 
@@ -55,13 +53,75 @@ scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+
 # Define the neural network model
 model = keras.Sequential([
-    keras.layers.Input(shape=(X_train_scaled.shape[1],)),
+    keras.layers.Input(shape=(X_train_scaled.shape[1],)),  # Input layer
+    # Hidden layer with 512 units and ReLU activation
     keras.layers.Dense(512, activation='relu'),
+    # Hidden layer with 216 units and ReLU activation
     keras.layers.Dense(216, activation='relu'),
+    # Output layer with linear activation for regression
     keras.layers.Dense(1, activation='linear')
 ])
 
-huber_loss = tf.keras.losses.Huber(dekta=1.0)
+# Compile the model with Huber loss
+# You can adjust the 'delta' parameter as needed
+huber_loss = tf.keras.losses.Huber(delta=1.0)
+# Use Huber loss for regression
 model.compile(optimizer='adam', loss=huber_loss)
+
+
+# Train the model
+model.fit(X_train_scaled, y_train, epochs=50, batch_size=64,
+          validation_data=(X_test_scaled, y_test))
+
+model_losses = pd.DataFrame(model.history.history)
+model_losses.plot()
+
+# Make predictions
+predictions = model.predict(X_test_scaled)
+
+mean_absolute_error(y_test, predictions)
+
+
+warnings.filterwarnings("ignore")
+
+venue = widgets.Dropdown(
+    options=df['venue'].unique().tolist(), description='Select Venue:')
+batting_team = widgets.Dropdown(
+    options=df['bat_team'].unique().tolist(),  description='Select Batting Team:')
+bowling_team = widgets.Dropdown(
+    options=df['bowl_team'].unique().tolist(),  description='Select Batting Team:')
+striker = widgets.Dropdown(
+    options=df['batsman'].unique().tolist(), description='Select Striker:')
+bowler = widgets.Dropdown(
+    options=df['bowler'].unique().tolist(), description='Select Bowler:')
+
+predict_button = widgets.Button(description="Predict Score")
+
+output = widgets.Output()
+
+
+def predict_score(b):
+    with output:
+        clear_output()  # Clear the previous output
+
+        # Decode the encoded values back to their original values
+        decoded_venue = venue_encoder.transform([venue.value])
+        decoded_batting_team = batting_team_encoder.transform(
+            [batting_team.value])
+        decoded_bowling_team = bowling_team_encoder.transform(
+            [bowling_team.value])
+        decoded_striker = striker_encoder.transform([striker.value])
+        decoded_bowler = bowler_encoder.transform([bowler.value])
+
+        input = np.array([decoded_venue,  decoded_batting_team,
+                         decoded_bowling_team, decoded_striker, decoded_bowler])
+        input = input.reshape(1, 5)
+        input = scaler.transform(input)
+        # print(input)
+        predicted_score = model.predict(input)
+        predicted_score = int(predicted_score[0, 0])
+
+        print(predicted_score)
